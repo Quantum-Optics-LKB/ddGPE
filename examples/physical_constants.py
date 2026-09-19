1472465ce9a87b66301cb9f7a01b4d037f165528
Killian <killian.guerrero@lkb.upmc.fr>
2026-09-19 19:41:43 +0200

    corrections on the reservoir contribution

--- examples/physical_constants.py
+++ examples/physical_constants.py
@@ -8,8 +8,13 @@ h_bar_SI = 1.05457182*1e-34
 rabi = 5.07/2/h_bar # (meV/h_bar) linear coupling (Rabi split)
 g0 = (1e-2) /h_bar  # (frequency/density) (meV/hbar)/(1/um^2) nonlinear coupling constant 
 gamma_exc, gamma_cav = 0.0015 /h_bar, 0.07 / 0.6571 / h_bar # (meV/h_bar) exc and ph linewidth 1microeV 69microeV  original value 0.07/h_bar
-gamma_res = 0.0015 /h_bar # (meV/h_bar) reservoir linewidth
 omega_exc = 1484.44 /h_bar # (meV/h_bar) exciton energy measured from the cavity energy #-0.5
 omega_cav = 1482.76 /h_bar # (meV/h_bar) cavity energy at k=0  original value: 1482.76 /h_bar
 n_cav = 3.54
 k_z = 27 # (1/µm) n_cav*omega_cav/c
+
+# Target reservoir/coherent LP blueshift ratio at steady state and k=0.
+beta = 0.49
+delta_cx = omega_cav - omega_exc
+X02 = 0.5 * (1 + delta_cx / (delta_cx**2 + 4 * rabi**2)**0.5)
+gamma_res = beta * X02 * gamma_exc # (1/ps) coherent-to-reservoir feeding rate
